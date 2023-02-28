540089a9a4dca51b31cc68472488d1d7a6c343c0
Vésper <nicolasalencar100@gmail.com>
2023-02-27 22:46:08 -0300

    atividade1

---
+++ exerc/atividade1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num, s;
+    printf("Digite um número:");
+    scanf("%d", &num);
+    s = num*2;
+    printf("%d", s);
+    return 0;
+}
